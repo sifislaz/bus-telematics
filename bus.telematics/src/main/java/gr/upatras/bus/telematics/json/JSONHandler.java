fd8af92e9a2bc70e416455836a166656c06e2c84
sifislaz <jlazanakis@gmail.com>
2022-05-24 22:25:56 +0300

    added JSON file parser

--- bus.telematics/src/main/java/gr/upatras/bus/telematics/json/JSONHandler.java
+++ bus.telematics/src/main/java/gr/upatras/bus/telematics/json/JSONHandler.java
@@ -1,8 +1,13 @@
 package gr.upatras.bus.telematics.json;
 
-import org.json.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
 import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class JSONHandler {
 	
@@ -13,10 +18,10 @@ public class JSONHandler {
 	}
 	
 	public static JSONArray editJSONArr(JSONArray jsa, int ind, String id, String value) {
-		JSONObject obj = jsa.getJSONObject(ind);
+		JSONObject obj = (JSONObject)jsa.get(ind);
 		obj.remove(id);
 		obj.put(id,value);
-		jsa.put(ind,obj);
+		jsa.add(ind,obj);
 		return jsa;
 	}
 	
@@ -30,5 +35,29 @@ public class JSONHandler {
 			e.printStackTrace();
 		}
 	}
+	
+	public static Hashtable<String,Hashtable<String,String>> readJSONFile(String path){
+		Hashtable<String,Hashtable<String,String>> jsonList = new Hashtable<String,Hashtable<String,String>>();
+		Hashtable<String, String> json = new Hashtable<String, String>();
+		try {
+			Object arr = new JSONParser().parse(new FileReader(path));
+			JSONArray jarr = (JSONArray)arr;
+			for(int i=0; i<jarr.size(); i++) {
+				JSONObject jo = (JSONObject)jarr.get(i);
+				json.put("id",jo.get("id").toString());
+				json.put("long", jo.get("long").toString());
+				json.put("lat", jo.get("lat").toString());
+				json.put("routeId", jo.get("routeId").toString());
+				jsonList.put(jo.get("id").toString(),json);
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		return jsonList;
+	}
+	
+	
 	
 }
